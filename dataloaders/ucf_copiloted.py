dde121f8cc1c482ea0099e537ace528cb3a99c5e
LunyCipres <sarahnoor12232@gmail.com>
2023-11-20 18:56:54 +0500

    merging codes -- attempt 1

---
+++ dataloaders/ucf_copiloted.py
@@ -0,0 +1,252 @@
+import os
+import pandas as pd
+import numpy as np
+import random
+from torchvision import transforms
+import cv2
+from torch.utils.data import Dataset, DataLoader
+import torch
+
+
+class Normalize(object):
+    def __call__(self, buffer):
+        buffer = buffer.astype(np.float32)
+        buffer /= 255.0
+        return buffer
+
+
+class ClipSubstractMean(Normalize):
+    def __call__(self, buffer):
+        buffer = super().__call__(buffer)
+        means = np.array((101.4, 97.7, 90.2))
+        buffer -= means
+        return buffer
+
+
+class RandomCrop(object):
+    def __init__(self, output_size=(112, 112), padding=10):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+        self.padding = padding
+
+    def __call__(self, buffer):
+        h, w = buffer.shape[1], buffer.shape[2]
+        new_h, new_w = self.output_size
+
+        top = np.random.randint(0, h - new_h + 2 * self.padding)
+        left = np.random.randint(0, w - new_w + 2 * self.padding)
+
+        padded_buffer = np.pad(buffer, ((0, 0), (self.padding, self.padding), (self.padding, self.padding), (0, 0)), mode='constant')
+        new_buffer = padded_buffer[:, top: top + new_h, left: left + new_w, :]
+
+        return new_buffer
+
+
+class CenterCrop(object):
+    def __init__(self, output_size=(112, 112), padding=10):
+        assert isinstance(output_size, (int, tuple))
+        if isinstance(output_size, int):
+            self.output_size = (output_size, output_size)
+        else:
+            assert len(output_size) == 2
+            self.output_size = output_size
+        self.padding = padding
+
+    def __call__(self, buffer):
+        h, w = buffer.shape[1], buffer.shape[2]
+        new_h, new_w = self.output_size
+
+        top = int(round(h - new_h + 2 * self.padding) / 2.)
+        left = int(round(w - new_w + 2 * self.padding) / 2.)
+
+        padded_buffer = np.pad(buffer, ((0, 0), (self.padding, self.padding), (self.padding, self.padding), (0, 0)), mode='constant')
+        new_buffer = padded_buffer[:, top: top + new_h, left: left + new_w, :]
+
+        return new_buffer
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, buffer):
+        if np.random.random() < self.p:
+            for i, frame in enumerate(buffer):
+                buffer[i] = cv2.flip(frame, flipCode=1)
+
+        return buffer
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, buffer):
+        # swap color axis because
+        # numpy image: batch_size x H x W x C
+        # torch image: batch_size x C X H X W
+        img = torch.from_numpy(buffer.transpose((3, 0, 1, 2)))
+
+        return img.float().div(255)
+
+
+class UCFDataset(Dataset):
+    r"""A Dataset for a folder of videos. Expects the directory structure to be
+    directory->[train/val/test]->[class labels]->[videos]. Initializes with a list
+    of all file names, along with an array of labels, with label being automatically
+    inferred from the respective folder names.
+
+        Args:
+            root_dir (str): path to n_frames_jpg folders.
+            info_list (str): path to annotation file.
+            split(str): whether create trainset. Default='train'.
+            clip_len (int): Determines how many frames are there in each clip. Defaults to 16.
+
+    """
+
+    def __init__(self, root_dir, info_list, split='train', clip_len=16):
+        self.root_dir = root_dir
+        self.clip_len = clip_len
+        self.landmarks_frame = pd.read_csv(info_list, delimiter=' ', header=None)
+        self.label_class_map = {}
+        with open('./Dataset/ucfTrainTestlist/classInd.txt') as f:
+            for line in f:
+                line=line.strip('\n').split(' ')
+                self.label_class_map[line[1]] = line[0]
+        self.split = split
+        if split == 'train':
+            self.transform = transforms.Compose(
+                [ClipSubstractMean(),
+                 RandomCrop(),
+                 RandomHorizontalFlip(),
+                 ToTensor()])
+        else:
+            self.transform = transforms.Compose(
+                [ClipSubstractMean(),
+                 CenterCrop(),
+                 ToTensor()])
+        # The following three parameters are chosen as described in the paper section 4.1
+        self.resize_height = 128
+        self.resize_width = 171
+        self.crop_size = 112
+
+    def __len__(self):
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, index):
+        # Loading and preprocessing.
+        video_path = self.landmarks_frame.iloc[index, 0]
+        # labels [0,100]
+        if self.landmarks_frame.shape[1] == 2:
+            labels = self.landmarks_frame.iloc[index, 1] - 1
+        else:
+            classes = video_path.split('/')[0]
+            labels = int(self.label_class_map[classes]) - 1
+        buffer = self.get_resized_frames_per_video(video_path)
+
+        if self.transform:
+            buffer = self.transform(buffer)
+
+        return buffer, torch.from_numpy(np.array(labels))
+
+    def get_resized_frames_per_video(self, video_path):
+        slash_rows = video_path.split('.')
+        dir_name = slash_rows[0]
+        video_jpgs_path = os.path.join(self.root_dir, dir_name)
+        # get the random continuous 16 frame
+        data = pd.read_csv(os.path.join(video_jpgs_path, 'n_frames'), delimiter=' ', header=None)
+        frame_count = data[0][0]
+        video_x = np.empty((self.clip_len, self.resize_height, self.resize_width, 3), np.dtype('float32'))
+        image_start = random.randint(1, abs(frame_count - self.clip_len))
+        for i in range(self.clip_len):
+            s = "%05d" % (i + image_start)
+            image_name = 'image_' + s + '.jpg'
+            image_path = os.path.join(video_jpgs_path, image_name)
+            tmp_image = cv2.imread(image_path)
+            tmp_image = cv2.resize(tmp_image, (self.resize_width, self.resize_height))
+            tmp_image = np.array(tmp_image).astype(np.float64)
+            tmp_image = tmp_image[:, :, ::-1]    # BGR -> RGB
+            video_x[i, :, :, :] = tmp_image
+
+        return video_x
+
+
+if __name__ == '__main__':
+    # usage
+    root_list = './Dataset/UCF101_n_frames/'
+    info_list = './Dataset/ucfTrainTestlist/testlist01.txt'
+
+    # trainUCF101 = UCFDataset(root_list, info_list,'test'
+    #                           )
+    # testUCF101 = UCFDataset(root_list, info_list,
+    #                          transform=transforms.Compose(
+    #                              [ClipSubstractMean(),
+    #                               CenterCrop(),
+    #                               ToTensor()]))
+
+    # dataloader = DataLoader(trainUCF101, batch_size=8, shuffle=True, num_workers=0)
+    test_dataloader = DataLoader(UCFDataset(root_dir='./Dataset/UCF101_n_frames',
+                                              info_list='./Dataset/ucfTrainTestlist/testlist01.txt',
+                                              split='test',
+                                              clip_len=16),
+                                 batch_size=8, shuffle=True,num_workers=0)
+
+    for i_batch, (images, targets) in enumerate(test_dataloader):
+        print(i_batch, images.size(), targets.size())
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+# Add these imports at the beginning of your file
+from sklearn.model_selection import train_test_split
+from torch.utils.data import WeightedRandomSampler
+
+# Add these methods to your UCFDataset class
+
+def remove_duplicates(self, buffer):
+    # Implement a method to remove duplicate frames
+    pass
+
+def normalize(self, buffer):
+    # Implement a method to normalize pixel values
+    pass
+
+def balance_classes(self):
+    # Implement a method to balance classes
+    pass
+
+def quality_control(self):
+    # Implement a method to check the quality of your data and labels
+    pass
+
+# Modify your __getitem__ method to include the new preprocessing steps
+def __getitem__(self, index):
+    # Loading and preprocessing.
+    video_path = self.landmarks_frame.iloc[index, 0]
+    # labels [0,100]
+    if self.landmarks_frame.shape[1] == 2:
+        labels = self.landmarks_frame.iloc[index, 1] - 1
+    else:
+        classes = video_path.split('/')[0]
+        labels = int(self.label_class_map[classes]) - 1
+    buffer = self.get_resized_frames_per_video(video_path)
+
+    buffer = self.remove_duplicates(buffer)
+    buffer = self.normalize(buffer)
+
+    if self.transform:
+        buffer = self.transform(buffer)
+
+    return buffer, torch.from_numpy(np.array(labels))
+
+# Modify your DataLoader to include data splitting and loading optimization
+train_data, test_data = train_test_split(UCFDataset(root_dir='./Dataset/UCF101_n_frames',
