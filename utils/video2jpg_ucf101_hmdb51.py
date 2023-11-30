848cad35ad541ded5a182e9bec886dc35f0b4e20
LunyCipres <sarahnoor12232@gmail.com>
2023-11-30 11:05:25 +0500

    Data loading optimization
    updated vid2jpeg code to cap framerate at 1fps to limit how many frames it retrieves.

--- utils/video2jpg_ucf101_hmdb51.py
+++ utils/video2jpg_ucf101_hmdb51.py
@@ -34,7 +34,7 @@ def class_process(dir_path, dst_dir_path, class_name):
         except:
             print(dst_directory_path)
             continue
-        cmd = 'ffmpeg -i \"{}\" -vf scale=-1:240 \"{}/image_%05d.jpg\"'.format(video_file_path, dst_directory_path)
+        cmd = 'ffmpeg -i \"{}\" -vf "fps=1,scale=-1:240" \"{}/image_%05d.jpg\"'.format(video_file_path, dst_directory_path)
         #视频的读取与逐帧图像保存
         print(cmd)
         subprocess.call(cmd, shell=True)
